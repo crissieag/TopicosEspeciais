c6c0a674af2625093ab49c2e0fcd581110528bdb
Alissonzinho <alissonscrok2018@gmail.com>
2022-05-01 21:05:53 -0300

    Atualização de branch

--- projeto/src/scripts/professor.js
+++ projeto/src/scripts/professor.js
@@ -1,4 +1,15 @@
 let matricula = document.getElementById('matricula');
+let agendarBtn = document.getElementById('agendarBtn');
+let agendaProva = document.getElementById('prova');
+let agendaAtividade = document.getElementById('atividade');
+let agendaAula = document.getElementById('aula');
+
+let modal = document.getElementById("modal");
+let close = document.getElementById("close");
+
+let modalContent = document.querySelector(".modal-content");
+let errorMsg = document.querySelector(".errorMsg");
+
 
 function mostraBotoes() {
     if (matricula.value == '') {
@@ -9,6 +20,16 @@ function mostraBotoes() {
     }
 }
 
-function mostraModal() {
-    
+function modalAgendamento() {
+    console.log("asijdasijd");
+    if (agendaAula.checked == true || agendaProva.checked == true || agendaAtividade.checked == true) {
+		modal.style.display = "block";
+		errorMsg.style.display = "none";
+	} else {
+		errorMsg.style.display = "block";
+	}
+}
+
+function fechaModal() {
+	modal.style.display = "none";
 }
