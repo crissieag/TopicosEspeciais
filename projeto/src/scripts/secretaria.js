3677a850bd7b21d4370aaaa0292243d85e6a7b36
RafaelGusto <rafael.gustojunior@gmail.com>
2022-05-02 17:23:37 -0300

    update secretaria

---
+++ projeto/src/scripts/secretaria.js
@@ -0,0 +1,19 @@
+function showModal() {
+    var element = document.getElementById("modal");
+    element.classList.add("show-modal");
+}
+
+function closeModal() {
+    var element = document.getElementById("modal");
+    element.classList.remove("show-modal");
+}
+
+function showModall() {
+    var element = document.getElementById("modall");
+    element.classList.add("show-modal");
+}
+
+function closeModall() {
+    var element = document.getElementById("modall");
+    element.classList.remove("show-modal");
+}
