aa1f59adf92e8d6bfd939ad163907c238d247250
FKRIK <fernandokrik@hotmail.com>
2022-06-26 22:42:27 -0300

    Menu lateral finalizado jQuery

--- projeto/src/scripts/menuLateral.js
+++ projeto/src/scripts/menuLateral.js
@@ -6,33 +6,33 @@ let main = document.getElementById("main");
 let expandeLi = document.getElementsByClassName("expandeLi");
 let logoutBtn = document.getElementById("logoutBtn");
 
-navIconOpen.addEventListener("click", () => {
-	menuLateral.style.transition = "0.5s";
-	menuLateral.style.width = "75px";
-	navUl.style.display = "block";
-	main.style.filter = "blur(1.5px)";
-	logoutBtn.style.display = "none";
-	if (menuLateral.style.width == "75px") {
-		navIconClose.style.display = "block";
-		navIconOpen.style.display = "none";
-		logoutBtn.style.display = "block";
-		logoutBtn.style.position = "relative";
-	}
-});
+// navIconOpen.addEventListener("click", () => {
+// 	menuLateral.style.transition = "0.5s";
+// 	menuLateral.style.width = "125px";
+// 	navUl.style.display = "block";
+// 	main.style.filter = "blur(1.5px)";
+// 	logoutBtn.style.display = "none";
+// 	if (menuLateral.style.width == "75px") {
+// 		navIconClose.style.display = "block";
+// 		navIconOpen.style.display = "none";
+// 		logoutBtn.style.display = "block";
+// 		logoutBtn.style.position = "relative";
+// 	}
+// });
 
-navIconClose.addEventListener("click", () => {
-	navIconOpen.style.display = "block";
-	menuLateral.style.transition = "0.5s";
-	menuLateral.style.width = "15px";
-	navUl.style.display = "none";
-	navIconClose.style.display = "none";
-	main.style.filter = "blur(0)";
-	logoutBtn.style.display = "none";
-});
+// navIconClose.addEventListener("click", () => {
+// 	navIconOpen.style.display = "block";
+// 	menuLateral.style.transition = "0.5s";
+// 	menuLateral.style.width = "15px";
+// 	navUl.style.display = "none";
+// 	navIconClose.style.display = "none";
+// 	main.style.filter = "blur(0)";
+// 	logoutBtn.style.display = "none";
+// });
 
-logoutBtn.addEventListener("click", () => {
-	window.location.replace("/projeto/login.html");
-});
+// logoutBtn.addEventListener("click", () => {
+// 	window.location.replace("/projeto/login.html");
+// });
 
 window.addEventListener("resize", () => {
 	if (window.matchMedia("(min-width: 1024px)").matches) {
